c2b6aa63899815f9f468748cad84c52079c0d229
Charlie Thomas <2198970T@student.gla.ac.uk>
2017-01-19 21:34:30 +0000

    Chapter 5 + excerises completed

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,9 +1,14 @@
 from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=120 ,unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
