3041d601426a3d0fb6df090a06c508cbdb8e76f9
Charlie Thomas <charlie.thomas321@gmail.com>
2017-03-20 23:02:14 +0000

    Fixed some errors for nucleus tests

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -38,28 +38,45 @@ def populate():
             "url":"http://flask.pocoo.org",
             "views": 37}]
 
-    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
-            "Django": {"pages": django_pages, "views": 64, "likes": 32},
-            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
+    cats = {
+        "Python": {
+            "pages": python_pages,
+            "views": 128,
+            "likes": 64
+        },
+        "Django": {
+            "pages": django_pages,
+            "views": 64,
+            "likes": 32
+        },
+        "Other Frameworks": {
+            "pages": other_pages,
+            "views": 32,
+            "likes": 16
+        }
+    }
 
 
-    for cat, cat_data in cats.iteritems():
+    for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
+
         for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"], p["views"])
+            add_page(c, p['title'], p['url'], p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views):
+
+def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
     p.save()
     return p
 
-def add_cat(name, views, likes):
+
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
     c.views = views
     c.likes = likes
